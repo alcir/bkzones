27efe637bfe98c8fd4c25cb95a808cf86e4b69bc
alcir <alciregi@gmail.com>
2013-08-13 11:44:20 +0200

    Create listallvmzfs.sh
    List all the ZFS associated to all the virtual machines

---
+++ stuff/listallvmzfs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+while read xuuid
+do
+
+  zoneuid=$xuuid
+
+  vmadm get $zoneuid | json zfs_filesystem
+
+  while read disk
+  do
+
+    echo $disk
+
+  done < <(vmadm get $zoneuid | json disks | grep zfs_filesystem | awk -F"\"" '{print $4}')
+
+  while read dataset
+  do
+
+    echo $dataset
+
+  done < <(vmadm get $zoneuid | json datasets | json -a)
+
+
+done < <(vmadm list -p -o uuid)
